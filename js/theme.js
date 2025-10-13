4eb7e5a7d8eb200dc1edc77f72566d4c55331f03
dejimania <kamilbalogun1@gmail.com>
2025-10-13 13:16:05 +0100

    Add dark/light mode setting

---
+++ js/theme.js
@@ -0,0 +1,17 @@
+const themeToggle = document.getElementById("theme-toggle");
+const currentTheme = localStorage.getItem("theme");
+
+// Apply saved theme
+if (currentTheme === "dark") {
+  document.body.classList.add("dark");
+  themeToggle.textContent = "🌞";
+}
+
+// Toggle theme on click
+themeToggle.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  const isDark = document.body.classList.contains("dark");
+
+  themeToggle.textContent = isDark ? "🌞" : "🌙";
+  localStorage.setItem("theme", isDark ? "dark" : "light");
+});
